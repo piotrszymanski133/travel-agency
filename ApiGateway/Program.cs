c4ec1bc5fedeaf00d35c7d5be148cd822d207671
piotrszymanski133 <63449627+piotrszymanski133@users.noreply.github.com>
2022-04-30 15:01:54 +0200

    Fix repository structure, add empty TripService

---
+++ ApiGateway/Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseCors(options =>
+    options.WithOrigins("http://localhost:8080")
+        .AllowAnyMethod()
+        .AllowAnyHeader());
+
+app.Run();
