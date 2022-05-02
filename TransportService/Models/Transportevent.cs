1c9669892ff59b5112891c835ec56ff109773c57
Magdalena-N <magdusiamn@gmail.com>
2022-05-02 14:15:00 +0200

    Added ORM

---
+++ TransportService/Models/Transportevent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TransportService.Models
+{
+    public partial class Transportevent
+    {
+        public long Id { get; set; }
+        public long TransportId { get; set; }
+        public int Places { get; set; }
+
+        public virtual Transport Transport { get; set; } = null!;
+    }
+}
