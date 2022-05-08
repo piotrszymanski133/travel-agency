7042cfff0b0b64e196f7c0544e1e92b318582c67
Magdalena-N <magdusiamn@gmail.com>
2022-05-08 16:31:33 +0200

    Fix uploading data to postgres

--- data/hotels.sql
+++ data/hotels.sql
@@ -27,10 +27,18 @@ CREATE TABLE HotelRooms (
 	ID smallint NOT NULL,
 	Hotel_ID smallint NOT NULL,
 	RoomType_ID smallint NOT NULL,
-	Quantity smallint NOT NULL,
 	PRIMARY KEY (ID),
 	CONSTRAINT Hotel_ID FOREIGN KEY(Hotel_ID) REFERENCES Hotels(ID),
 	CONSTRAINT RoomType_ID FOREIGN KEY(RoomType_ID) REFERENCES HotelRoomTypes(ID)
+);
+
+CREATE TABLE HotelRoomAvailabilities (
+    ID smallint NOT NULL,
+    HotelRoom_ID smallint NOT NULL,
+    Quantity smallint NOT NULL,
+    Date date NOT NULL,
+    PRIMARY KEY (ID),
+    CONSTRAINT HotelRoom_ID FOREIGN KEY(HotelRoom_ID) REFERENCES HotelRooms(ID)
 );
 
 CREATE TABLE Tours (
